1da565e5b9e8ee2294cb8adca0d8d4b5492f999b
Axel Irribarren <axelirribarren@gmail.com>
2026-01-15 20:33:57 -0300

    fixing problems with ios connections

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,19 +1,21 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-// CHANGE THIS IP TO YOUR COMPUTER'S LOCAL IP
-const API_URL = 'http://192.168.123.39:8001';
 
-const api = axios.create({
-    baseURL: API_URL,
+export const api = axios.create({
+    baseURL: 'https://lfdr3cm-axelitoow-8081.exp.direct',
 });
 
-api.interceptors.request.use(async (config) => {
-    const token = await SecureStore.getItemAsync('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
+api.interceptors.request.use(
+    async (config) => {
+        const token = await SecureStore.getItemAsync('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
 
 export default api;
