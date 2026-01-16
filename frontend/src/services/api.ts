2ea4c5cac384cb68d23b847a60b609417dddf51c
Axel Irribarren <axelirribarren@gmail.com>
2026-01-16 20:45:34 -0300

    Icon.png working correctly

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
 // CHANGE THIS IP TO YOUR COMPUTER'S LOCAL IP
-const API_URL = 'http://192.168.123.39:8001';
+const API_URL = 'https://new-buckets-tie.loca.lt';
 
 const api = axios.create({
     baseURL: API_URL,
